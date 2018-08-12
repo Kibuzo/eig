434c5773d1c805f8cae6ab3db74ef33c042f690b
kibuzo <kibuzo@debian>
2018-08-12 16:20:20 +0200

    as

--- ServUO/Scripts/Items/Equipment/Armor/BaseArmor.cs
+++ ServUO/Scripts/Items/Equipment/Armor/BaseArmor.cs
@@ -2530,7 +2530,7 @@ namespace Server.Items
             int Absorbed = (int)(HalfAr + HalfAr * Utility.RandomDouble());
 
             //damageTaken -= Absorbed; l'ho commentato io
-			damageTaken*=Arperc;
+			damageTaken=int(damageTaken*Arperc);
 
             if (damageTaken < 0) 
                 damageTaken = 0;
