8a3ae8888e3f7ef515bce3b682ca7b55fe7ec60d
kibuzo <kibuzo@debian>
2018-08-16 15:50:32 +0200

    Bf1

--- ServUO/Scripts/Mobiles/PlayerMobile.cs
+++ ServUO/Scripts/Mobiles/PlayerMobile.cs
@@ -1037,7 +1037,7 @@ namespace Server.Mobiles
             base.ComputeResistances();
 			// Questa parte l'ho aggiunta io (Kibuzo)
 			Resistances[0] = (int)((RawStr-100.0)/2.0); //era in resistances[0]+=
-			if (Race==Race.Elf) Resistances[0] = (int)((RawStr-100.0)/2.0)-10.0;
+			if (Race==Race.Elf) Resistances[0] = (int)((RawStr-100.0)/2.0-10.0);
 			//fine parte mia
             for (int i = 1; i < Resistances.Length; ++i)
             {
@@ -1082,7 +1082,7 @@ namespace Server.Mobiles
 			int minp = GetMinResistance((ResistanceType)0)-(int)((RawStr-100.0)/2.0);
 			int maxp = (int)((RawStr-100.0)/2.0)+GetMaxResistance((ResistanceType)0);
 			if (Race == Race.Elf)
-				maxp -= 10.0;
+				maxp -= 10;
 			if (maxp < minp)
 			{
 				maxp = minp;
