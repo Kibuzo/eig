6b760cf4d8f5270d229b20cf19bbf8d5cc92f977
kibuzo <kibuzo@debian>
2018-08-16 22:09:35 +0200

    Aggiunta la fuffaroAI, expect bugfixes

--- ServUO/Scripts/Mobiles/Normal/Vegano.cs
+++ ServUO/Scripts/Mobiles/Normal/Vegano.cs
@@ -8,7 +8,7 @@ namespace Server.Mobiles
 	{
 		[Constructable]
 		public Vegan()
-			: base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+			: base(FuffaroAI, FightMode.Closest, 10, 1, 0.2, 0.4)
 		{
 			bool Fem=false;
 			SpeechHue = 18;
